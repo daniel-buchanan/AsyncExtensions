604d42a03fb2be4248953bdd773e67854b0faed8
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2024-12-17 15:23:41 +1300

    refactor: tidy up exception

--- src/AsyncExtensions/CapturedAsyncException.cs
+++ src/AsyncExtensions/CapturedAsyncException.cs
@@ -2,9 +2,5 @@ using System;
 
 namespace Common.AsyncExtensions;
 
-public class CapturedAsyncException : Exception
-{
-    public CapturedAsyncException(string message) : base(message) { }
-    
-    public CapturedAsyncException(string message, Exception innerException) : base(message, innerException) { }
-}
+public class CapturedAsyncException(string message, Exception innerException) : 
+    Exception(message, innerException);
