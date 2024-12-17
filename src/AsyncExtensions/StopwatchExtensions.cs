4fcd06d701a52a7dd1932ec4b51e51c90b808f74
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2024-12-17 16:05:44 +1300

    chore: add greenery

--- src/AsyncExtensions/StopwatchExtensions.cs
+++ src/AsyncExtensions/StopwatchExtensions.cs
@@ -6,24 +6,50 @@ namespace Common.AsyncExtensions;
 
 public static class StopwatchExtensions
 {
-    public static Stopwatch Time(this Stopwatch self, Action task)
+    /// <summary>
+    /// Execute and time a given method and return a result representing the timing of the provided method.
+    /// </summary>
+    /// <param name="self">The <see cref="Stopwatch" /> instance to use.</param>
+    /// <param name="task">The <see cref="Action"/> or method to time.</param>
+    /// <returns>A new <see cref="StopwatchResult"/> representing the timing of the method.</returns>
+    public static StopwatchResult Time(this Stopwatch self, Action task)
         => self.TimeAsync(() => { task(); return Task.CompletedTask; }).WaitFor();
     
-    public static T Time<T>(this Stopwatch self, Func<T> task)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="self"></param>
+    /// <param name="task"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static StopwatchResult<T> Time<T>(this Stopwatch self, Func<T> task)
         => self.TimeAsync(() => Task.FromResult(task())).WaitFor();
 
-    public static async Task<Stopwatch> TimeAsync(this Stopwatch self, Func<Task> task)
+    /// <summary>
+    /// Execute and time a given async method and return a result representing the timing of the provided method.
+    /// </summary>
+    /// <param name="self">The <see cref="Stopwatch" /> instance to use.</param>
+    /// <param name="task">The async <see cref="Func{Task}"/> or method to time.</param>
+    /// <returns>A new <see cref="StopwatchResult"/> representing the timing of the method.</returns>
+    public static async Task<StopwatchResult> TimeAsync(this Stopwatch self, Func<Task> task)
     {
         await self.TimeAsync(async () => { await task(); return Task.FromResult(true); });
-        return self;
+        return StopwatchResult.FromStopwatch(self, task.Method.Name);
     }
     
-    public static async Task<T> TimeAsync<T>(this Stopwatch self, Func<Task<T>> task)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="self"></param>
+    /// <param name="task"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static async Task<StopwatchResult<T>> TimeAsync<T>(this Stopwatch self, Func<Task<T>> task)
     {
         self.Reset();
         self.Start();
         var result = await task();
         self.Stop();
-        return result;
+        return StopwatchResult.FromStopwatch(self, task.Method.Name, result);
     }
 }
---
+++ src/AsyncExtensions/StopwatchResult.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Diagnostics;
+
+namespace Common.AsyncExtensions;
+
+/// <summary>
+/// Result from a <see cref="StopwatchExtensions.Time"/> method.
+/// </summary>
+public class StopwatchResult
+{
+    protected StopwatchResult(TimeSpan timeSpan, string signature)
+    {
+        Elapsed = timeSpan;
+        ElapsedMilliseconds = timeSpan.TotalMilliseconds;
+        MethodSignature = signature;
+    }
+    
+    /// <summary>Gets the total elapsed time measured by the stopwatch, in milliseconds.</summary>
+    /// <returns>A read-only long integer representing the total number of milliseconds measured by the stopwatch.</returns>
+    public double ElapsedMilliseconds { get; }
+    
+    /// <summary>Gets the total elapsed time measured by the stopwatch.</summary>
+    /// <returns>A read-only <see cref="T:System.TimeSpan" /> representing the total elapsed time measured by the stopwatch.</returns>
+    public TimeSpan Elapsed { get; }
+    
+    /// <summary>
+    /// The signature of the method that was timed.
+    /// </summary>
+    public string MethodSignature { get; }
+    
+    /// <summary>
+    /// Create a <see cref="StopwatchResult"/> from a <see cref="Stopwatch"/> instance.
+    /// </summary>
+    /// <param name="stopwatch">The <see cref="Stopwatch"/> instance to use.</param>
+    /// <param name="signature">The signature of the method that was timed.</param>
+    /// <returns>A new <see cref="StopwatchResult"/> representing the method that was timed.</returns>
+    public static StopwatchResult FromStopwatch(Stopwatch stopwatch, string signature) 
+        => new(stopwatch.Elapsed, signature);
+    
+    /// <summary>
+    /// Create a <see cref="StopwatchResult{T}"/> from a <see cref="Stopwatch"/> instance.
+    /// </summary>
+    /// <param name="stopwatch">The <see cref="Stopwatch"/> instance to use.</param>
+    /// <param name="signature">The signature of the method that was timed.</param>
+    /// <param name="result">The result from the timed method.</param>
+    /// <typeparam name="T">The type of the response from the method.</typeparam>
+    /// <returns>A new <see cref="StopwatchResult{T}"/> containing the result of the timed method and timing details.</returns>
+    public static StopwatchResult<T> FromStopwatch<T>(Stopwatch stopwatch, string signature, T result) 
+        => new(stopwatch.Elapsed, signature, result);
+}
+
+/// <summary>
+/// Represents the result of a method that was timed by a <see cref="Stopwatch"/>.
+/// </summary>
+/// <typeparam name="T">The type of the result from the timed method.</typeparam>
+public class StopwatchResult<T> : StopwatchResult
+{
+    protected internal StopwatchResult(TimeSpan timeSpan, string signature, T result) : 
+        base(timeSpan, signature) 
+        => Result = result;
+
+    /// <summary>
+    /// The result of the method that was timed.
+    /// </summary>
+    public T Result { get; }
+}
