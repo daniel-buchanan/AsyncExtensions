2094c03bcf9a0f7f652490d9d476299e4ee7367c
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2024-12-17 13:44:42 +1300

    refactor: consolidate time methods

--- src/AsyncExtensions/StopwatchExtensions.cs
+++ src/AsyncExtensions/StopwatchExtensions.cs
@@ -1,30 +1,28 @@
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Common.AsyncExtensions;
 
 public static class StopwatchExtensions
 {
-    public static Stopwatch StartNow(this Stopwatch stopwatch)
-    {
-        stopwatch.Start();
-        return stopwatch;
-    }
-
     public static Stopwatch Time(this Stopwatch self, Action task)
+        => self.TimeAsync(() => { task(); return Task.CompletedTask; }).WaitFor();
+    
+    public static T Time<T>(this Stopwatch self, Func<T> task)
+        => self.TimeAsync(() => Task.FromResult(task())).WaitFor();
+
+    public static async Task<Stopwatch> TimeAsync(this Stopwatch self, Func<Task> task)
     {
-        self.Reset();
-        self.Start();
-        task();
-        self.Stop();
+        await self.TimeAsync(async () => { await task(); return Task.FromResult(true); });
         return self;
     }
-
-    public static T Time<T>(this Stopwatch self, Func<T> task)
+    
+    public static async Task<T> TimeAsync<T>(this Stopwatch self, Func<Task<T>> task)
     {
         self.Reset();
         self.Start();
-        var result = task();
+        var result = await task();
         self.Stop();
         return result;
     }
