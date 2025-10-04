b6b26e4475995c218e5d80534972a9f60ddac120
Daniel Buchanan <dkb@outlook.co.nz>
2025-10-04 15:31:29 +1300

    chore: add docs

--- test/AsyncExtensions.Tests/EnumerableExtensionsTests.cs
+++ test/AsyncExtensions.Tests/EnumerableExtensionsTests.cs
@@ -140,4 +140,73 @@ public class EnumerableExtensionsTests
         // Assert
         result.Should().BeAssignableTo<string[]>();
     }
+    
+    [Fact]
+    public async Task EnumerableToEnumerableSucceeds()
+    {
+        // Arrange
+        async Task<IEnumerable<string>> Method()
+        {
+            await Task.Delay(10);
+            return Enumerable.Repeat("a", 4);
+        }
+
+        // Act
+        var result = await Method().ToEnumerableAsync();
+
+        // Assert
+        result.Should().BeAssignableTo<IEnumerable<string>>();
+    }
+    
+    [Fact]
+    public async Task ArrayToEnumerableSucceeds()
+    {
+        // Arrange
+        async Task<string[]> Method()
+        {
+            await Task.Delay(10);
+            return ["a", "b", "c"];
+        }
+
+        // Act
+        var result = await Method().ToEnumerableAsync();
+
+        // Assert
+        result.Should().BeAssignableTo<IEnumerable<string>>();
+    }
+    
+    [Fact]
+    public async Task ListToEnumerableSucceeds()
+    {
+        // Arrange
+        async Task<IEnumerable<string>> Method()
+        {
+            await Task.Delay(10);
+            return Enumerable.Repeat("a", 4);
+        }
+
+        // Act
+        var result = await Method().ToEnumerableAsync();
+
+        // Assert
+        result.Should().BeAssignableTo<IEnumerable<string>>();
+    }
+    
+    [Fact]
+    // ReSharper disable once InconsistentNaming
+    public async Task IListToEnumerableSucceeds()
+    {
+        // Arrange
+        async Task<IList<string>> Method()
+        {
+            await Task.Delay(10);
+            return new List<string> { "a", "b", "c" };
+        }
+
+        // Act
+        var result = await Method().ToEnumerableAsync();
+
+        // Assert
+        result.Should().BeAssignableTo<IEnumerable<string>>();
+    }
 }
